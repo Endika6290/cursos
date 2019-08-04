c65d7b3158b2a6463bb078b18198dec4f73f41fd
Endika Santamaria <endika.santamaria@stubhub.com>
2019-08-04 13:26:04 +0200

    Parte 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Contador extends Component{
+    constructor(props){
+        super(props)
+        this.state ={ contador:this.props.contadorInicial}
+        setInterval(() =>{
+            this.setState({contador: this.state.contador +1})
+        },1000)
+    }
+    render() {
+        return <ContadorNum numero ={this.state.contador}/>
+    }
+}
+
+Contador.defaultProps={
+    contadorInicial:0
+}
+
+class ContadorNum extends Component{
+    render() {
+        console.log('ContadorNumeo Render()')
+        return (
+        <span>{this.props.numero}</span>
+        );
+    }
+}
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                 <p>Primer Componente</p>
+                <Contador contadorInicial={100}/>
+            </div>
+        );
+    }
 }
 
 export default App;
